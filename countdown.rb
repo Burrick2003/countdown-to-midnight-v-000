4e100a3580b904523912f10879aa06cc0b092361
Blakeley Bond <burrick2003@yahoo.com>
2017-10-28 04:44:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,9 +5,8 @@ def countdown(seconds_to_midnight)
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
     seconds_to_midnight -= 1
-
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(seconds_to_midnight)
@@ -16,5 +15,5 @@ def countdown_with_sleep(seconds_to_midnight)
     seconds_to_midnight -= 1
     sleep 1
   end
-  puts "HAPPY NEW YEAR!"
+  "HAPPY NEW YEAR!"
 end
